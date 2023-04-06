2c639099be6d54ef17133e2548cdf050817b0020
DevDyna <78147916+DevDyna@users.noreply.github.com>
2023-04-06 22:42:33 +0200

    Update SD packager.bat

--- SD packager.bat
+++ SD packager.bat
@@ -14,27 +14,27 @@ mkdir distance-fancy
 cd ..
 ::copy files to SD/Pack-fast
 ::from package
-robocopy /E Scaffolding-Delta-main\package\fast SD\Pack-fast
+robocopy /E package\fast SD\Pack-fast
 ::from modules
-robocopy /E Scaffolding-Delta-main\emit\fast\assets SD\Pack-fast\assets
-robocopy /E Scaffolding-Delta-main\distance\fast\assets SD\Pack-fast\assets
+robocopy /E emit\fast\assets SD\Pack-fast\assets
+robocopy /E distance\fast\assets SD\Pack-fast\assets
 ::copy files to SD/Pack-fancy
 ::from package
-robocopy /E Scaffolding-Delta-main\package\fancy SD\Pack-fancy
+robocopy /E package\fancy SD\Pack-fancy
 ::from modules
-robocopy /E Scaffolding-Delta-main\ctm\assets SD\CTM\assets
-robocopy /E Scaffolding-Delta-main\distance\fancy\assets SD\Pack-fancy\assets
-robocopy /E Scaffolding-Delta-main\emit\fancy\assets SD\Pack-fancy\assets
+robocopy /E ctm\assets SD\CTM\assets
+robocopy /E distance\fancy\assets SD\Pack-fancy\assets
+robocopy /E emit\fancy\assets SD\Pack-fancy\assets
 
 ::copy files to SD/CTM
-robocopy /E Scaffolding-Delta-main\ctm SD\CTM
+robocopy /E ctm SD\CTM
 
 ::copy files to SD/distance-fast
-robocopy /E Scaffolding-Delta-main\distance\fast SD\distance-fast
+robocopy /E distance\fast SD\distance-fast
 ::copy files to SD/distance-fancy
-robocopy /E Scaffolding-Delta-main\distance\fancy SD\distance-fancy
+robocopy /E distance\fancy SD\distance-fancy
 
 ::copy files to SD/emit-fancy
-robocopy /E Scaffolding-Delta-main\emit\fast SD\emit-fast
+robocopy /E emit\fast SD\emit-fast
 ::copy files to SD/emit-fast
-robocopy /E Scaffolding-Delta-main\emit\fancy SD\emit-fancy
+robocopy /E emit\fancy SD\emit-fancy
