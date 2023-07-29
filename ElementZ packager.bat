5ca2802f9446ddbd767b970e0d28918a7fba7745
DevDyna <78147916+DevDyna@users.noreply.github.com>
2023-07-29 23:54:15 +0200

    Update and rename MOD packager.bat to ElementZ packager.bat

--- ElementZ packager.bat
+++ ElementZ packager.bat
@@ -1,3 +1,5 @@
 @ECHO OFF
+::Remove old file
 IF EXIST "ElementZ-#VERSION#.jar" DEL "ElementZ-#VERSION#.jar"
+::Create .jar file with all file selected
 jar cf "ElementZ-#VERSION#.jar" assets data META-INF pack.mcmeta things
