4f0164d78ba0a8edbff2649cd4d7b20186f3b4af
DevDyna <78147916+DevDyna@users.noreply.github.com>
2023-07-30 16:34:38 +0200

    Update ElementZ packager.bat

--- ElementZ packager.bat
+++ ElementZ packager.bat
@@ -2,4 +2,4 @@
 ::Remove old file
 IF EXIST "ElementZ-#VERSION#.jar" DEL "ElementZ-#VERSION#.jar"
 ::Create .jar file with all file selected
-jar cf "ElementZ-#VERSION#.jar" assets data META-INF pack.mcmeta things
+jar cf "ElementZ-#VERSION#.jar" assets data META-INF pack.mcmeta pack.png things
